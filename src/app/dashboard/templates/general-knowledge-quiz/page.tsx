b2ed3b1bcff5bfeaabd9ee0b7108778ea01df5aa
alpaca <alpacayt01@gmail.com>
2024-04-27 08:46:37 +0800

    Address Error

--- src/app/dashboard/templates/general-knowledge-quiz/page.tsx
+++ src/app/dashboard/templates/general-knowledge-quiz/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import QuizTemplate from "@/components/templates/QuizTemplate";
 
 const Page = async () => {
--- src/utils/load-ffmpeg.ts
+++ src/utils/load-ffmpeg.ts
@@ -2,11 +2,20 @@ import { FFmpeg } from "@ffmpeg/ffmpeg";
 import { toBlobURL } from "@ffmpeg/util";
 
 export default async function loadFfmpeg(): Promise<FFmpeg> {
-  const ffmpeg = new FFmpeg();
-  const baseURL = "https://unpkg.com/@ffmpeg/core@0.12.6/dist/umd";
-  await ffmpeg.load({
-    coreURL: await toBlobURL(`${baseURL}/ffmpeg-core.js`, "text/javascript"),
-    wasmURL: await toBlobURL(`${baseURL}/ffmpeg-core.wasm`, "application/wasm"),
-  });
-  return ffmpeg;
+  if (typeof window !== "undefined") {
+    const ffmpeg = new FFmpeg();
+    const baseURL = "https://unpkg.com/@ffmpeg/core@0.12.6/dist/umd";
+    await ffmpeg.load({
+      coreURL: await toBlobURL(`${baseURL}/ffmpeg-core.js`, "text/javascript"),
+      wasmURL: await toBlobURL(
+        `${baseURL}/ffmpeg-core.wasm`,
+        "application/wasm"
+      ),
+    });
+    return ffmpeg;
+  } else {
+    throw new Error(
+      "Window is not defined. FFmpeg can only be loaded in a browser environment."
+    );
+  }
 }
